92c94ecc36d3f2d359c18049d5187c2aaab13509
Kubafqt <bakubaf@gmail.com>
2021-11-02 21:28:20 +0100

    Minor changes to code readibility

--- snake/spells/BombShot.cs
+++ snake/spells/BombShot.cs
@@ -7,60 +7,47 @@ using System.Drawing;
 
 namespace snake_sandbox01
 {
-    class BombShot
-    {
-        public static List<BombShot> bombyShotList = new List<BombShot>();
+   class BombShot
+   {
+      public static List<BombShot> bombyShotList = new List<BombShot>();
 
-        //enum Directions
-        //{
-        //    X,
-        //    Y,
-        //}
+      public string direction = "";
+      public int speed = 0;
+      public int x, y;
+      public Point position;
+      public bool goThroughOnce = false;
 
-        public string direction = "";
-        public int speed = 0;
-        public int x, y;
-        public Point position;
-        public bool goThroughOnce = false;
+      public BombShot(Point position, string direction, int speed)
+      {
+         this.position = position;
+         this.direction = direction;
+         this.speed = speed;
+      }
 
-        public BombShot(Point position, string direction, int speed)
-        {
-            this.position = position;
-            this.direction = direction;
-            this.speed = speed;
-            //Form1.blockArr[position.X, position.Y] = "bombshot";
-        }
+      public static void BombMovement(BombShot bomb)
+      {
+         int x = bomb.position.X;
+         int y = bomb.position.Y;
+         if (Form1.blockArr[x, y] == "hardblock")
+         {
+            Explode.explosions.Add(new Explode(4, 50, (bomb.position.X + Explode.smerDictX[bomb.direction]) * Form1.sizeX, (bomb.position.Y + Explode.smerDictY[bomb.direction]) * Form1.sizeY, Color.DarkOrange));
+            RemoveBlock(bomb);
+            RemoveBomb(bomb);
+         }
+      }
 
-        public static void BombMovement(BombShot bomb)
-        {
-            int x = bomb.position.X;
-            int y = bomb.position.Y;
-            if (Form1.blockArr[x, y] == "hardblock")
-            {
-                Explode.explosions.Add(new Explode(4, 50, (bomb.position.X + Explode.smerDictX[bomb.direction]) * Form1.sizeX, (bomb.position.Y + Explode.smerDictY[bomb.direction]) * Form1.sizeY, Color.DarkOrange));
-                Form1.blockArr[x, y] = "";
-                Form1.blockPointList.Remove(bomb.position);
-                RemoveBomb(bomb);
-            }
-        }
+      public static void RemoveBlock(BombShot bomb)
+      {
+         int x = bomb.position.X;
+         int y = bomb.position.Y;
+         Form1.blockArr[x, y] = "";
+         Form1.blockPointList.Remove(bomb.position);
+      }
 
-        public string message(int a, int b = 10)
-        {
-            int x = a + b;
-            string vystup = x.ToString();
-            return vystup;
-        }
+      public static void RemoveBomb(BombShot bomb)
+      {
+         bombyShotList.Remove(bomb);
+      }
 
-
-        public static void RemoveBlock()
-        {
-
-        }
-
-        public static void RemoveBomb(BombShot bomb)
-        {
-            bombyShotList.Remove(bomb);
-        }
-
-    }
+   }
 }
